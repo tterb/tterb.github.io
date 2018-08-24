666599457bfbf8c895102911481626db026bbd59
tterb <bstevensondev@gmail.com>
2018-08-23 22:17:26 -0700

    :shower: Cleanup unused and commented code

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -104,15 +104,14 @@
   (function ($) {
     "use strict";
     window.sr = ScrollReveal({ duration: 1000, reset: false });
+    window.sr2 = ScrollReveal({ duration: 2000, reset: false });
     if ($('body.landing').length) {
-      sr.reveal('#one .inner', 10);
-      sr.reveal('#two .inner', 10);
+      sr.reveal('#one .inner', 20);
+      sr.reveal('#two .inner', 20);
+      sr.reveal('.tile', 20);
+    } else if ($('body.blog').length) {
+      sr.reveal('post-card', 20);
     }
-    else if ($('.post').length) {
-      sr.reveal('#back-to-top', 25);
-    }
-    // sr.reveal('#three .tile', 25);
-    // sr.reveal('#three', 10);
     $('.filter li').on('click', function() {
       var $this = $(this),
       isActive = $this.hasClass( 'active' ),
@@ -122,13 +121,11 @@
       }
       $this.toggleClass('active');
     });
+    
+    $('.search-overlay').on('click', function() {
+      $('body').removeClass('search-overlay');
+    });
   }(jQuery));
-
-  // Asychronous Google-font loading
-  $(function() {
-    var $fonts = 'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i|Spectral:300,300i,500,500i,700,700i|Source+Code+Pro:300,400,600,700';
-    document.createStyleSheet ? document.createStyleSheet($fonts) : $('head').append($('<link rel="stylesheet" href="' + $fonts + '" type="text/css" media="screen" />'))
-  })
 
   // Improve code-block formatting
   $(document).ready(function() {
@@ -191,90 +188,4 @@
       removalDelay: 400
     });
   });
-  
-  // var filterizd = $('.filter-container').filterizr({
-  //   animationDuration: 0.5,    // in seconds
-  //   filter: 'all',             // Initial filter
-  //   callbacks: { 
-  //     onFilteringStart: function() { },
-  //     onFilteringEnd: function() { alignTiles() },
-  //     onShufflingStart: function() { },
-  //     onShufflingEnd: function() { alignTiles() },
-  //     onSortingStart: function() { },
-  //     onSortingEnd: function() { alignTiles() }
-  //   },
-  //   controlsSelector: '',      // Selector for custom controls
-  //   delay: 0,                  // Transition delay in ms
-  //   delayMode: 'progressive',  // 'progressive' or 'alternate'
-  //   easing: 'ease-out',
-  //   filterOutCss: {            // Filtering out animation
-  //     // position: 'relative',
-  //     opacity: 0,
-  //     margin: '0 0.025em',
-  //     transform: 'translate3d(0)'
-  //     // transform: 'scale(0.5)'
-  //   },
-  //   filterInCss: {             // Filtering in animation
-  //     // position: 'relative',
-  //     opacity: 0,
-  //     margin: '0 0.025em',
-  //     transform: 'translate3d(0)'
-  //     // transform: 'scale(1)',
-  //   },
-  //   layout: 'sameSize',        // See layouts
-  //   multifilterLogicalOperator: 'or',
-  //   selector: '.filter-container',
-  //   setupControls: true // Should be false if controlsSelector is set
-  // });
-  
-  // $('.tile').mouseenter(function() {
-  //   // console.log('mouseover');
-  //   tiles = getValues();
-  //   target = $(this);
-  //   targetIndex = 0;
-  //   $(tiles).each(function() {
-  //     if(target == $(this).element) {
-  //       targetIndex = $(this).index;
-  //     }
-  //   });
-  //   console.log(targetIndex);
-  //   var mod = parseInt(targetIndex/2);
-  //   $('.tile').each(function(index) {
-  //     if(mod < parseInt(index/2)) {
-  //       tiles[index].y += 100;
-  //       $(this).css('transform', 'translateY('+tiles[index].y+')');
-  //     }
-  //   });
-  // });
-  
-  // function getValues() {
-  //   var tiles = [];
-  //   $('.tile').each(function(index) {
-  //     var matrix = $(this).css('transform').replace(/[^0-9\-.,]/g, '').split(',');
-  //     var tile = {
-  //       index: index,
-  //       element: $(this),
-  //       x: parseFloat(matrix[12] || matrix[4]),
-  //       y: parseFloat(matrix[13] || matrix[5])
-  //     };
-  //     tiles.push(tile);
-  //   });
-  //   return tiles;
-  // }
-  
-  // alignTiles();
-  // 
-  // function alignTiles(row) {
-  //   row = row || 2;
-  //   var tiles = $('.tile');
-  //   $('.tile.top').removeClass('top');
-  //   for(var i = 0; i < tiles.length; i++) {
-  //     // tiles.eq(i).css('transform', '');
-  //     if(i < row) {
-  //       tiles.eq(i).addClass('top');
-  //     }
-  //   }
-  // }
-  
-  // $('.design-tiles .tile').poptrox()
 })(jQuery);
