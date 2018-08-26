84c7c19a20d8d75d5c8fc4e87c49de4467b8a9e8
tterb <bstevensondev@gmail.com>
2018-08-25 17:05:50 -0700

    :arrow_up: Improve blog post 'back-to-top' functionality

--- _assets/js/local/scroll-top.js
+++ _assets/js/local/scroll-top.js
@@ -1,21 +1,30 @@
-if ($("#back-to-top").length) {
-  var scrollTrigger = 200,
-  checkOffset = function() {
-    var b = $(document).height() - $(window).scrollTop()
-    $(window).scroll(function() {
+if ($('#back-to-top').length) {
+  var offset = 1400;
+  var scrollTrigger = 200;
+  checkOffset = () => {
+    var pos = $(document).height() - $(window).scrollTop();
+    var bottom = $(document).height() - offset;
+    $(window).scroll(() => {
       if ($(window).scrollTop() > scrollTrigger) {
-        $("#back-to-top").addClass("show")
+        $('#back-to-top').addClass('show');
+        if (pos > offset) {
+          $('#back-to-top').css({ position: 'fixed' });
+          $('#back-to-top').css({ transform: 'translateY(0)' });
+        } else {
+          $('#back-to-top').css({ position: 'absolute' });
+          $('#back-to-top').css({ transform: `translateY(${bottom}px)` });
+        }
       } else {
-        $("#back-to-top").removeClass("show")
+        $('#back-to-top').removeClass('show');
       }
     });
   };
   checkOffset();
-  $(window).on("scroll", function() {
-    checkOffset()
+  $(window).on('scroll', () => {
+    checkOffset();
   });
-  $("#back-to-top").on("click", function(a) {
-    a.preventDefault();
-    $("html,body").animate({ scrollTop: 0 }, 700);
+  $('#back-to-top').on('click', (e) => {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: 0 }, 700);
   });
 };
